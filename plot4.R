0e822da875e9d8f2958e0345c33c655555a7df4a
Kyun-Seop Bae <ksbae@acr.kr>
2014-12-07 23:53:47 +0900

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -17,6 +17,7 @@ plot(Sub_metering_1, xlab="", ylab="Energy sub metering", type="l", xaxt="n")
 lines(Sub_metering_2, col="red")
 lines(Sub_metering_3, col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c("black","red","blue"), cex=0.9, bty="n")
+axis(1, at=c(1, Len/2, Len), labels=c("Thu","Fri","Sat"))
 
 plot(Global_reactive_power, xlab="datetime", type="l", xaxt="n")
 axis(1, at=c(1, Len/2, Len), labels=c("Thu","Fri","Sat"))
